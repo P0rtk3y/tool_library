7aee1c4cc02b655a842aaa7b86611008f371fbfd
P0rtk3y <llama.p0rtk3y@gmail.com>
2019-08-17 20:30:36 -0700

    updates application controller with inheritable log in methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 class ApplicationController < ActionController::Base
+  def current_user
+    @library = Library.find_by_id(session[:library_id])
+  end
 
-  
+  def logged_in?
+    !!session[:library_id]
+  end
+
+  def require_login
+    unless logged_in?
+      flash[:message] = "Please log in"
+      reidrect_to root_path
+    end
+  end
+
 end
