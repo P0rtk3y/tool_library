12ceafa8727bf81238abfcb5268c5f5a0c998bd6
P0rtk3y <llama.p0rtk3y@gmail.com>
2019-08-18 13:48:35 -0700

    adds borrower controller methods

--- app/controllers/borrowers_controller.rb
+++ app/controllers/borrowers_controller.rb
@@ -1,8 +1,10 @@
 class BorrowersController < ApplicationController
   def index
+    @borrowers = Borrower.order('name ASC')
   end
 
   def show
+
   end
 
   def new
@@ -10,5 +12,21 @@ class BorrowersController < ApplicationController
   end
 
   def create
-  end 
+    @borrower = current_user.new_borrowers.build(borrower_params)
+    if @borrower.save
+      redirect_to borrowers_path
+    else
+      flash[:message] = "Borrower exists!"
+      render :new
+    end
+  end
+
+private
+
+
+  def borrower_params
+    params.require(:borrower).permit(:name)
+  end
+
+
 end
--- app/models/borrower.rb
+++ app/models/borrower.rb
@@ -2,4 +2,5 @@ class Borrower < ApplicationRecord
   has_many :loans
   has_many :tools, through: :loans
 
+  validates :name, uniqueness: true
 end
