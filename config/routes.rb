8f717b79e812814f4e85f8664f5792c86e2565e8
P0rtk3y <llama.p0rtk3y@gmail.com>
2019-08-17 18:28:01 -0700

    adds session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/signup' => 'libraries#new'
+  get '/login' =>'sessions#new'
+  post '/login' => 'sessions#create'
+
   resources :libraries
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
